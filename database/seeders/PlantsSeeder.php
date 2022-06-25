7d3d1a2308ca071b38580db1d27475d81c52b31c
Kacper Płócienniczak <kacperplocienniczak20@gmail.com>
2022-06-25 22:35:23 +0200

    add new seeder

--- database/seeders/PlantsSeeder.php
+++ database/seeders/PlantsSeeder.php
@@ -16,28 +16,10 @@ class PlantsSeeder extends Seeder
      */
     public function run()
     {
-        $faker = Factory::create();
-
         DB::table('plants')->truncate();
 
-        $positions = ['Bezpośrednie światło','Światło jasne, rozproszone','Półcień i cień'];
-
-        for($j = 0; $j<1;$j++)
-        {
-            $plants = [];
-            for($i = 0; $i < 20; $i++)
-            {
-                $plants[] =[
-                    'name'=> $faker->name(),
-                    'latinName' => $faker->words($faker->numberBetween(2, 5), true),
-                    'position'=> $positions[rand(0,2)],
-                    'soil' => $faker->slug(2),
-                    'watering'=> $faker->slug(2),
-                    'humidity'=> $faker->slug(2),
-                    'imgUrl'=> $faker->imageUrl(100,100, 'plants'),
-                ];
-            }
-        }
-        DB::table('plants')->insert($plants);
+        $path = base_path() . '/database/plants.sql';
+        $sql = file_get_contents($path);
+        DB::unprepared($sql);
     }
 }
